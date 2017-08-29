e2867d334e9f27a7fac87817e5b6f14e7eca49da
Lucien Rae <lucienraegentil@gmail.com>
2017-08-29 18:29:35 +1000

    Gusman intro page idea

--- Gusman/main.py
+++ Gusman/main.py
@@ -3,9 +3,13 @@ import time
 
 app = Flask(__name__)
 
-@app.route("/")
-def main():
-	return render_template("index.html", current_time=str(time.time()))
+@app.route("/gusman")
+def gusman():
+	return render_template("gusman.html", current_time=str(time.time()))
+
+@app.route("/gusmanapp")
+def gusmanapp():
+	return render_template("gusmanapp.html", current_time=str(time.time()))
 
 if __name__ == "__main__":
 	app.run(debug=True)
