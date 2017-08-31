899b92bd3f93f989434ed0f4ab724d191d37862a
Lucien Rae <lucienraegentil@gmail.com>
2017-08-31 18:48:06 +1000

    Form attempts

--- Gusman/main.py
+++ Gusman/main.py
@@ -1,15 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import time
+import sys
 
 app = Flask(__name__)
+
+reports = []
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+	if request.method == "GET":
+		return render_template("index.html", current_time=str(time.time()))
+	elif request.method == "POST":
+		if report_name in request.form:
+			report_id = len(reports)
+			report_name = request.form["report_name"]
+		else:
+			raise "oh no"
+
+@app.route("/output", methods=["GET"])
+def output():
+	return render_template("output.html", current_time=str(time.time()), reports=reports)
 
 @app.route("/gusman")
 def gusman():
 	return render_template("gusman.html", current_time=str(time.time()))
 
-@app.route("/gusmanapp")
-def gusmanapp():
-	return render_template("gusmanapp.html", current_time=str(time.time()))
-
 if __name__ == "__main__":
 	app.run(debug=True)
+	print(reports)
